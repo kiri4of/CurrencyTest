796827e29ce77bbddff75ea767f7cfb3ef040e5b
Kiri4of <kopak7890@gmail.com>
2025-02-06 19:29:42 +0000

    init

---
+++ CurrencyTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyTestTask
+//
+//  Created by Kiri4of on 06/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
